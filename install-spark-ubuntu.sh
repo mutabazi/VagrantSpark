ee9cdbcadf4ba9695edd786eda4791f1bb9583a3
mutabazi <mutabazi@users.noreply.github.com>
2016-08-18 20:57:07 -0400

    Update install-spark-ubuntu.sh

--- install-spark-ubuntu.sh
+++ install-spark-ubuntu.sh
@@ -3,3 +3,7 @@ sudo mkdir /usr/local/src/spark
 sudo tar xvf /usr/local/src/spark-2.0.0-bin-hadoop2.7.tgz -C /usr/local/src/spark/
 cd /usr/local/src/spark/spark-2.0.0/
 sudo build/sbt assembly
+sudo echo "export SPARK_HOME=/usr/local/src/spark/spark-2.0.0" >> .bashrc
+. .bashrc
+sudo echo "export PATH=$SPARK_HOME/bin:$PATH" >> .bashrc
+. .bashrc
