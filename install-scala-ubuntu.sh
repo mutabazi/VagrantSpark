33abec62b696944d1a58749b4329a6e1f90bdcee
mutabazi <mutabazi@users.noreply.github.com>
2016-08-18 21:10:13 -0400

    Update install-scala-ubuntu.sh

--- install-scala-ubuntu.sh
+++ install-scala-ubuntu.sh
@@ -5,3 +5,9 @@ sudo echo "export SCALA_HOME=/usr/local/src/scala/scala-2.11.8" >> .bashrc
 . .bashrc
 sudo echo "export PATH=$SCALA_HOME/bin:$PATH" >> .bashrc
 . .bashrc
+
+# Install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
