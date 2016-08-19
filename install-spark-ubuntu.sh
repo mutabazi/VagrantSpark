92ebb404d115dec02e927669275ada60d9ed5031
mutabazi <mutabazi@users.noreply.github.com>
2016-08-18 20:46:00 -0400

    Update install-spark-ubuntu.sh

--- install-spark-ubuntu.sh
+++ install-spark-ubuntu.sh
@@ -1,5 +1,5 @@
-sudo wget http://apache.mirror.anlx.net/spark/spark-1.6.0/spark-1.6.0.tgz -P /usr/local/src/
+sudo wget http://apache.mirror.anlx.net/spark/spark-2.0.0/spark-2.0.0-bin-hadoop2.7.tgz -P /usr/local/src/
 sudo mkdir /usr/local/src/spark
-sudo tar xvf /usr/local/src/spark-1.6.0.tgz -C /usr/local/src/spark/
-cd /usr/local/src/spark/spark-1.6.0/
+sudo tar xvf /usr/local/src/spark-2.0.0-bin-hadoop2.7.tgz -C /usr/local/src/spark/
+cd /usr/local/src/spark/spark-2.0.0/
 sudo build/sbt assembly
