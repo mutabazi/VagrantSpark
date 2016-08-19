70b126782e8f3e28bd2eed5dde037f32478ebe91
mutabazi <mutabazi@users.noreply.github.com>
2016-08-18 20:35:49 -0400

    Update install-scala-ubuntu.sh

--- install-scala-ubuntu.sh
+++ install-scala-ubuntu.sh
@@ -1,7 +1,7 @@
-sudo wget http://www.scala-lang.org/files/archive/scala-2.10.4.tgz -P /usr/local/src/
+sudo wget http://www.scala-lang.org/files/archive/scala-2.11.8.tgz -P /usr/local/src/
 sudo mkdir /usr/local/src/scala
-sudo tar xvf /usr/local/src/scala-2.10.4.tgz -C /usr/local/src/scala/
-sudo echo "export SCALA_HOME=/usr/local/src/scala/scala-2.10.4" >> .bashrc
+sudo tar xvf /usr/local/src/scala-2.11.8.tgz -C /usr/local/src/scala/
+sudo echo "export SCALA_HOME=/usr/local/src/scala/scala-2.11.8" >> .bashrc
 . .bashrc
 sudo echo "export PATH=$SCALA_HOME/bin:$PATH" >> .bashrc
 . .bashrc
