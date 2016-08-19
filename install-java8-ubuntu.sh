99cc26ae5ea709589ce996fe93c1f08b77f56734
mutabazi <mutabazi@users.noreply.github.com>
2016-08-18 21:28:55 -0400

    Update install-java8-ubuntu.sh

--- install-java8-ubuntu.sh
+++ install-java8-ubuntu.sh
@@ -1,10 +1,12 @@
 # Install Oracle JAVA 8 (JDK/JRE 8u101) on Ubuntu and LinuxMint systems using PPA
+sudo apt-get -y update
+sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:webupd8team/java
 sudo apt-get -y update
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
   sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+sudo apt-get -y install oracle-java8-installer
 # Webupd8 ppa repository also provides a package to set environment variables, Install this package using the following command:
-sudo apt-get install -y oracle-java8-set-default
+sudo apt-get -y install oracle-java8-set-default
